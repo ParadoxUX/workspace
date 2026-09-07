3307c14463c74b28723ff2ac307b2888fdf37030
kurava <k8716145@gmail.com>
2026-09-07 11:32:47 +0400

    test api

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("я запустился!");
+import { createServer, IncomingMessage, ServerResponse } from "node:http";
+
+const port = 8080;
+
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
+  res.setHeader("Content-type", "application/json; charset=utf-8");
+
+  res.end(
+    JSON.stringify({
+      message: "Тест АПИ! Привет!",
+      timestamp: new Date().toISOString(),
+    })
+  );
+});
+
+server.listen(port, () => {
+  console.log(`Сервер запущен по адресу http://localhost:${port}`);
+});
