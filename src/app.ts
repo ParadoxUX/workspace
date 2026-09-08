5e9657175dd56941f7d1825647f4125cadae637c
kurava <k8716145@gmail.com>
2026-09-08 13:43:47 +0400

    feat(API): добавили контролеры

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
 import { createServer, IncomingMessage, ServerResponse } from "node:http";
-import { posts, Post } from "./data.ts";
+import { getPosts, getPostById, notFound } from "./controllers.js";
 
 const port = 8000;
 
 const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-  res.setHeader("Content-Type", "application/json; charset=utf-8");
-  /* Указали, что сервер будет отдавать JSON */
+  const url = req.url || "/";
+  const method = req.method || "GET";
 
-  res.end(
-    JSON.stringify({
-      message: "Тест АПИ! Привет!",
-      timestamp: new Date().toISOString(),
-    })
-  );
+  console.log(`${method} ${url}`);
+  const [path, queryString] = url.split("?");
+
+  if (method === "GET") {
+    if (path === "/") {
+      getPosts(req, res);
+      return;
+    }
+
+    const match = path.match(/^\/posts\/([a-zA-Z0-9]+)$/);
+    if (match) {
+      const id = match[1];
+      getPostById(req, res, { id });
+      return;
+    }
+  }
+
+  notFound(req, res);
 });
 
 server.listen(port, () => {
-  console.log(`Сервер запущен на порту http://localhost:${port} `);
+  console.log(`Сервер запущен на порту http://localhost:${port}`);
 });
